66e69377985e7775ca7468f079cc512f66fd17e5
Quanalogy <aalsing@gmail.com>
2016-08-10 12:26:22 +0200

    Added test for avoiding duplicate email

--- backend/tests/controllers/user/testUserCreate.ts
+++ backend/tests/controllers/user/testUserCreate.ts
@@ -17,10 +17,12 @@ describe("", function() {
         mongoose.connection.db.dropDatabase(done);
     });
 
+    // Define test data.
+    const userEmail = "a@valid.email.com";
+    const userEmailUppercase = "A@valid.email.com";
+    const userPassword = "testT1!s";
+
     it("Can create a user in an empty database.", function(done) {
-        // Define test data.
-        const userEmail = "a@valid.email.com";
-        const userPassword = "testT1!s";
 
         Users.count({}).exec().then( (numberOfUsers) => {
             expect(numberOfUsers).toBe(0);
@@ -34,5 +36,20 @@ describe("", function() {
         }).then( () => {
             done();
         });
+    });
+
+    it("Can not differentiate between upper- and lowercase email", (done) => {
+        createUser(userEmail, userPassword).then(fullfilled => { // this should be ok - empty database
+            return createUser(userEmailUppercase, userPassword);
+        }, rejected => {    // Database error
+            fail("Is the database running? Cannot create user!");
+            done();
+        }).then(success => {    // Damn we can create an user with the same email!!
+            fail("Could create the user with the same uppercase email!!");
+            done();
+        }, failure => {     // Good the request was rejected
+            done();
+        });
+
     });
 });
