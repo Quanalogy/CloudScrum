51cd9e665fa9fd884ac39a83e5ec3c0c25200454
Magnus Aagaard Sørensen <mgsdk@users.noreply.github.com>
2016-08-22 15:46:39 +0200

    Alphabetic order.

--- backend/controllers/user/controller.user.utility.ts
+++ backend/controllers/user/controller.user.utility.ts
@@ -2,26 +2,6 @@ import Promise = require("bluebird");
 
 import {getUser} from "./userControllerRead";
 
-export function checkPass(email: string, password: string): Promise<boolean>{
-    return Promise.try<boolean>(() => {
-        // Check if the data is valid.
-        if (!email) {
-            return false;
-        } else if (!password) {
-            return false;
-        }
-
-        // Attempt to find the user.
-        return getUser(email).then((user) => {
-            // Attempt to perform the validation.
-            return user.checkPassword(password);
-        }, () => {
-            // No user located.
-            return false;
-        });
-    });
-}
-
 export function changePass(email: string, oldPassword: string, newPassword: string): Promise<boolean>{
     return Promise.try<boolean>(() => {
         if (!oldPassword || !newPassword || !email){
@@ -38,6 +18,26 @@ export function changePass(email: string, oldPassword: string, newPassword: stri
             }
         }, (err) => {
             throw new Error(err);
+        });
+    });
+}
+
+export function checkPass(email: string, password: string): Promise<boolean>{
+    return Promise.try<boolean>(() => {
+        // Check if the data is valid.
+        if (!email) {
+            return false;
+        } else if (!password) {
+            return false;
+        }
+
+        // Attempt to find the user.
+        return getUser(email).then((user) => {
+            // Attempt to perform the validation.
+            return user.checkPassword(password);
+        }, () => {
+            // No user located.
+            return false;
         });
     });
 }
