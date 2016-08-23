84205d328c6051a1d8c71dc95092219a2f831c88
Magnus Aagaard Sørensen <mgsdk@users.noreply.github.com>
2016-08-23 16:26:12 +0200

    Missing file from last commit.

--- backend/routes/projects.routes.ts
+++ backend/routes/projects.routes.ts
@@ -2,16 +2,28 @@ import {Router, Request,Response} from "express";
 import {JSONSendItemResponse, JSONSendError} from "../utilities/JSONSender";
 import {create} from "../controllers/project/create";
 import jwt = require("jsonwebtoken");
+import {getProject} from "../controllers/project/read";
 
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-    JSONSendItemResponse(res, true)
+    //getProject(req.body)
+    JSONSendItemResponse(res, true);
 });
 
 router.post("/", (req: Request, res: Response) => {
     // Extract the user.
-    const user = jwt.decode(req.headers["authorization"], "L33tWallahWallah");
+    console.log(req.headers["authorization"]);
+    let user;
+
+    if (req.headers["authorization"] && req.headers["authorization"].split(" ")[0].toLowerCase() === "bearer") {
+        console.log("here");
+        user = jwt.decode(req.headers["authorization"].split(" ")[1]);
+
+        console.log(jwt.decode(req.headers["authorization"].split(" ")[1], "L33tWallahWallah"));
+    }
+
+    console.log(user);
 
     create(req.body.item, user.email)
         .then(() => {
